65fce1df487afd2de32e9d3ebc487874e71436bc
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-03-09 12:15:45 +0000

    Fixed issue with get_filenames in io_utils.

--- sbc_ngs/demultiplex.py
+++ sbc_ngs/demultiplex.py
@@ -97,7 +97,7 @@ def demultiplex(barcodes, in_dir, min_length, max_read_files, out_dir,
     read_thread = ReadThread(write_queue, out_dir)
     read_thread.start()
 
-    filenames = io_utils.get_filenames(in_dir, max_read_files)
+    filenames = io_utils.get_filenames([in_dir], max_read_files)
 
     results = [pool.apply_async(_bin_seqs, args=(fle,
                                                  min_length,
